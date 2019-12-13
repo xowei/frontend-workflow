d0e9d1ab03b6743344b433fa5307cca8e3abe99e
xowei <wei@xowei.tw>
2019-12-14 02:00:15 +0800

    avoided error breaks browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ const $ = require('gulp-load-plugins')({
 var prod    = !!$.util.env.production;
 console.log('production: ' + $.util.env.production);
 
-function pug() {
+function pug(done) {
   var options = {
     removeComments: true,
     collapseWhitespace: true,
@@ -29,21 +29,21 @@ function pug() {
   };
   
   return src(['src/*.pug', '!src/includes/**/*.pug'])
-    .pipe($.plumber())
     .pipe($.pug({ basedir: 'src/',
                   pretty: true }))
     .pipe(prod ? $.htmlmin(options) : $.util.noop())
+    .on('error', done)
     .pipe(dest('dist/'));
 }
 
-function css() {
+function css(done) {
   var processors = [
     require('postcss-import'),
+    require('postcss-mixins'),
+    require('postcss-nested'),
     require('postcss-cssnext')({
       browsers: ['IE 9', 'last 5 versions', 'Firefox 14', 'Opera 11.1'],
     }),
-    require('postcss-mixins'),
-    require('postcss-nested'),
     require('rucksack-css'),
     require('lost'),
     require('postcss-reporter')({ clearMessages: true })
@@ -58,15 +58,16 @@ function css() {
   };
 
   return src('src/css/styles.css')
-    .pipe($.plumber())
     .pipe($.sourcemaps.init({ loadMaps: true }))
     .pipe($.postcss(processors, { parser: sugarss }))
     .pipe(prod ? $.cssnano(cssnanoConfig) : $.util.noop())
     .pipe($.sourcemaps.write('.'))
-    .pipe(dest('dist/css/'));
+    .on('error', done)
+    .pipe(dest('dist/css/'))
+    .pipe($.browserSync.stream());
 }
 
-function js() {
+function js(done) {
   var b = $.browserify({
     debug: true,
     entries: 'src/js/scripts.js',
@@ -76,21 +77,23 @@ function js() {
     .on('error', $.util.log)
     .pipe($.source('scripts.js'))
     .pipe($.buffer())
-    .pipe($.plumber())
     .pipe($.sourcemaps.init({ loadMaps: true }))
     .pipe(prod ? $.uglify() : $.util.noop())
     .pipe($.sourcemaps.write('.'))
+    .on('error', done)
     .pipe(dest('dist/js/'));
 }
 
-function images(){
+function images(done){
   return src('src/images/*.*', { allowEmpty: true })
     .pipe($.imagemin({ progressive: true }))
+    .on('error', done)
     .pipe(dest('dist/images/'));
 };
 
-function fonts(){
+function fonts(done){
   return src('src/fonts/', { allowEmpty: true })
+    .on('error', done)
     .pipe(dest('dist/fonts/'));
 };
 
